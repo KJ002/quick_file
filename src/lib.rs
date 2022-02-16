20da6a06bf2de83f103882c08a5e94d94afdc740
James Butcher <jamesbutcher167@gmail.com>
2022-02-16 12:45:05 +0000

    Improve error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,41 @@
-use std::{fs::{write, read_to_string}, panic::panic_any};
+use std::{
+    fs::{read_to_string, write},
+    panic::panic_any,
+};
 
 pub struct ConfigManager {
-    pub name: String,
-    pub root_path: String
+    pub root_path: String,
 }
 
 impl ConfigManager {
     pub fn new(name: String) -> Self {
         Self {
-            name: name.clone(),
             root_path: match std::env::consts::OS {
                 "linux" | "macos" => match std::env::var("HOME") {
-                    Ok(x) => format!("{}/.{}", x, name),
+                    Ok(home) => format!("{}/.{}", home, name),
                     Err(err) => panic_any(err),
                 },
                 "windows" => match std::env::var("APPDATA") {
-                    Ok(x) => format!("{}/{}", x, name),
+                    Ok(appdata) => format!("{}/{}", appdata, name),
                     Err(err) => panic_any(err),
                 },
                 _ => unimplemented!(),
-            }
+            },
         }
     }
 
-    pub fn write(&self, file_name: &str, content: &str) {
-        write(format!("{}/{}", self.root_path, file_name), content).unwrap()
+    pub fn write(&self, file_name: &str, content: &str) -> std::io::Result<()> {
+        write(format!("{}/{}", self.root_path, file_name), content)
     }
 
-    pub fn read(&self, file_name: &str) -> String {
+    pub fn read(&self, file_name: &str) -> std::io::Result<String> {
         let path = format!("{}/{}", self.root_path, file_name);
-        match read_to_string(path) {
-            Ok(content) => content,
-            Err(err) => panic_any(err)
-        }
+        read_to_string(path)
     }
 
-    pub fn swap_file(&self, file_name: &str, content: &str) -> String {
+    pub fn swap_file(&self, file_name: &str, content: &str) -> std::io::Result<String> {
         let previous_content = self.read(file_name);
-        self.write(file_name, content);
+        self.write(file_name, content)?;
         previous_content
     }
 }
